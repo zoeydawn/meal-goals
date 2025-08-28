5b39cdef9ef3b44633ed87acbdda17489fe875bc
zoey <z@zoey.ninja>
2025-08-28 20:08:27 +0000

    updated type

--- types/calendarTypes.ts
+++ types/calendarTypes.ts
@@ -1,7 +1,1 @@
-export type DateObject = {
-  dateString: string // 'YYYY-MM-DD'
-  day: number // Day of the month (1–31)
-  month: number // Month of the year (1–12)
-  year: number // Year
-  timestamp: number // Unix timestamp (milliseconds)
-}
+export { DateData as DateObject } from 'react-native-calendars'
